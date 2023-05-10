9b2b260560dc069f1c12c007663c49b98344f865
Matheus Mendonça <matthmen16@gmail.com>
2023-05-09 22:03:31 -0300

    feat: js data

---
+++ index.js
@@ -0,0 +1,12 @@
+const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez',]
+
+function render() {
+  let output = ''
+
+  for(let month of months) {
+    output += `<div>${month}</>`
+  }
+
+  return output
+}
+app.querySelector('main').innerHTML = render()
